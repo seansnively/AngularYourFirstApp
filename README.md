ba94f876e6f685b69999d34c99e837ea638dec56
seansnively <gator123ss@gmail.com>
2019-06-27 10:31:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-59hhbd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-59hhbd)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+  @Input() improduct;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
