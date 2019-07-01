667372cd4abb1560ce45f64a2b3a45825dcea6b3
seansnively <gator123ss@gmail.com>
2019-07-01 09:58:06 -0400

    Completed routing tutorial

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
+  constructor(private route: ActivatedRoute) {
 
-  constructor() { }
+   }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => { this.product = products[+params.get('productId')];
+    });
   }
 
 }
